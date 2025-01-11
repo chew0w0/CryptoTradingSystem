88040f756b43c6ed41464918b8c661049e55aece
cyl <chewyeeling77@gmail.com>
2025-01-11 23:46:26 +0800

    Initial commit - Spring Boot project setup

---
+++ src/test/java/com/cyl/crypto_trading_system/CryptoTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyl.crypto_trading_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
